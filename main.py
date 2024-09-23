cc26db92c5eb5c3f335cc374635140fee635b52f
naughtyfox220 <f.merten@mail.ru>
2024-09-23 12:44:05 +0300

    Shablon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import sys
 import json
+import asyncio
 
 from pydantic import BaseModel
 from logging import StreamHandler
@@ -8,9 +9,29 @@ from loguru import logger
 
 from models import Point, Warrior, Archer, Varvar, Army
 
-logger.add("debug.log", format="{time} {level} {message}", level="ERROR")
+logger.add("debug.log", format="{time} {level} {message}", level="INFO")
 
-def main():
+
+async def output(army_win: Army, army_fail: Army):
+
+    result_army = army_win.model_dump()
+    result_army['loss'] = army_win.count - len(list(army_win.units.keys()))
+    result_army.pop('count')
+    result_army.pop('units')
+    print("\n")
+    logger.info(f"Army {army_win.name} won")
+    print("\n")
+    loss_army = army_fail.model_dump()
+    loss_army['loss'] = army_fail.count - len(list(army_fail.units.keys()))
+    logger.info(f"Loss: {loss_army['loss']}")
+    loss_army.pop('count')
+    loss_army.pop('units')
+    res_data = {"win": result_army, "loss": loss_army}
+    with open("res_data.json", 'w') as json_file:
+        json.dump(res_data, json_file)
+
+
+async def main():
     main_map = Point(x=30, y=30)
     print("Enter count of characters: ")
     wariors = list([])
@@ -30,6 +51,8 @@ def main():
             X = random.randint(1, main_map.x)
             Y = random.randint(1, main_map.y)
             pnt = Point(x=X, y=Y)
+            t1 = pnt.x > 0 and pnt.x < main_map.x
+            t2 = pnt.y > 0 and pnt.y < main_map.y
         print("Enter type of character (w/a/v):", end=' ')
         s = random.choice(['w', 'a', 'v'])
 
@@ -58,24 +81,9 @@ def main():
     army1 = Army(id=1, name=army1_name, count=0, units=dict())
 
     # Добавляем юнитов в 1-ую армию
-    while w1 > 0:
-        ind = random.randint(0, len(wariors) - 1)
-        army1.units[wariors[ind].id] = wariors[ind]
-        army1.count += 1
-        wariors.pop(ind)
-        w1 -= 1
-    while a1 > 0:
-        ind = random.randint(0, len(archers) - 1)
-        army1.units[archers[ind].id] = archers[ind]
-        army1.count += 1
-        archers.pop(ind)
-        a1 -= 1
-    while v1 > 0:
-        ind = random.randint(0, len(varvars) - 1)
-        army1.units[varvars[ind].id] = varvars[ind]
-        army1.count += 1
-        varvars.pop(ind)
-        v1 -= 1
+    army1.add_unit(w1, wariors)
+    army1.add_unit(a1, archers)
+    army1.add_unit(v1, varvars)
 
     # Вводим данные для 2-ой армии
     print("Enter number of warriors in 2st army: ")
@@ -91,25 +99,9 @@ def main():
     army2 = Army(id=2, name=army2_name, count=0, units=dict())
 
     # Добавляем юнитов в 2-ую армию
-    while w2 > 0:
-        ind = random.randint(0, len(wariors) - 1)
-        army2.units[wariors[ind].id] = wariors[ind]
-        army2.count += 1
-        wariors.pop(ind)
-        w2 -= 1
-    while a2 > 0:
-        ind = random.randint(0, len(archers) - 1)
-        army2.units[archers[ind].id] = archers[ind]
-        army2.count += 1
-        archers.pop(ind)
-        a2 -= 1
-    while v2 > 0:
-        ind = random.randint(0, len(varvars) - 1)
-        army2.units[varvars[ind].id] = varvars[ind]
-        army2.count += 1
-        varvars.pop(ind)
-        v2 -= 1
-
+    army2.add_unit(w2, wariors)
+    army2.add_unit(a2, archers)
+    army2.add_unit(v2, varvars)
 
     while len(list(army1.units.keys())) > 0 and len(list(army2.units.keys())) > 0:
         keys1 = list(army1.units.keys())
@@ -125,7 +117,7 @@ def main():
                     result_army['loss'] = army2.count - len(list(army2.units.keys()))
                     result_army.pop('count')
                     result_army.pop('units')
-                    print("\nArmy 2 won")
+                    logger.info("\nArmy 2 won")
                     loss_army = army1.model_dump()
                     loss_army['loss'] = army1.count - len(list(army1.units.keys()))
                     loss_army.pop('count')
@@ -138,7 +130,7 @@ def main():
                     result_army['loss'] = army1.count - len(list(army1.units.keys()))
                     result_army.pop('count')
                     result_army.pop('units')
-                    print("\nArmy 1 won")
+                    logger.info("\nArmy 1 won")
                     loss_army = army2.model_dump()
                     loss_army['loss'] = army2.count - len(list(army2.units.keys()))
                     loss_army.pop('count')
@@ -146,17 +138,16 @@ def main():
                     res_data = {"win": result_army, "loss": loss_army}
                     with open("res_data.json", 'w') as json_file:
                         json.dump(res_data, json_file)
-                print('Some')
                 return
             logger.error(e)
 
-        res = army1.units[army1_id].attack(main_map, army2.units[army2_id])
+        res = await army1.units[army1_id].attack(main_map, army2.units[army2_id])
         if res:
-            print(f"Unit {army1_id} from army 1 has attacked unit {army2_id} from army 2")
-            print(f"Health of unit {army1_id} from army 1: {army1.units[army1_id].health}")
-            print(f"Health of unit {army2_id} from army 2: {army2.units[army2_id].health}")
+            logger.info(f"Unit {army1_id} from {army1.name} has attacked unit {army2_id} from {army2.name}")
+            logger.info(f"Health of unit {army1_id} from {army1.name}: {army1.units[army1_id].health}")
+            logger.info(f"Health of unit {army2_id} from {army2.name}: {army2.units[army2_id].health}")
             if res >= 0:
-                print(f"Unit {army2_id} has failed")
+                logger.info(f"Unit {army2_id} has failed")
                 army2.units.pop(army2_id)
 
         move_x = random.randint(-army1.units[army1_id].coord.x, main_map.x - army1.units[army1_id].coord.x)
@@ -183,19 +174,19 @@ def main():
         except IndexError as e:
             if not keys1 or not keys2:
                 if not keys1:
-                    print("\nArmy 2 won")
+                    await output(army2, army1)
                 else:
-                    print("\nArmy 1 won")
+                    await output(army1, army2)
                 return
             logger.error(e)
 
-        res = army2.units[army2_id].attack(main_map, army1.units[army1_id])
+        res = await army2.units[army2_id].attack(main_map, army1.units[army1_id])
         if res:
-            print(f"Unit {army2_id} from army 2 has attacked unit {army1_id} from army 1")
-            print(f"Health of unit {army2_id} from army 2: {army2.units[army2_id].health}")
-            print(f"Health of unit {army1_id} from army 1: {army1.units[army1_id].health}")
+            logger.info(f"Unit {army2_id} from {army2.name} has attacked unit {army1_id} from {army1.name}")
+            logger.info(f"Health of unit {army2_id} from {army2.name}: {army2.units[army2_id].health}")
+            logger.info(f"Health of unit {army1_id} from {army1.name}: {army1.units[army1_id].health}")
             if res >= 0:
-                print(f"Unit {army1_id} has failed")
+                logger.info(f"Unit {army1_id} has failed")
                 army1.units.pop(army1_id)
 
         try:
@@ -217,32 +208,10 @@ def main():
         keys1 = list(army1.units.keys())
         keys2 = list(army2.units.keys())
         if not keys1:
-            result_army = army2.model_dump()
-            result_army['loss'] = army2.count - len(list(army2.units.keys()))
-            result_army.pop('count')
-            result_army.pop('units')
-            print("\nArmy 2 won")
-            loss_army = army1.model_dump()
-            loss_army['loss'] = army1.count - len(list(army1.units.keys()))
-            loss_army.pop('count')
-            loss_army.pop('units')
-            res_data = {"win": result_army, "loss": loss_army}
-            with open("res_data.json", 'w') as json_file:
-                json.dump(res_data, json_file)
+            await output(army2, army1)
         elif not keys2:
-            result_army = army1.model_dump()
-            result_army['loss'] = army1.count - len(list(army1.units.keys()))
-            result_army.pop('count')
-            result_army.pop('units')
-            print("\nArmy 1 won")
-            loss_army = army2.model_dump()
-            loss_army['loss'] = army2.count - len(list(army2.units.keys()))
-            loss_army.pop('count')
-            loss_army.pop('units')
-            res_data = {"win": result_army, "loss": loss_army}
-            with open("res_data.json", 'w') as json_file:
-                json.dump(res_data, json_file)
+            await output(army1, army2)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 import random
 from abc import abstractmethod
+from loguru import logger
 
 from pydantic import BaseModel
 
+logger.add("debug.log", format="{time} {level} {message}", level="INFO")
 
 class Point(BaseModel):
     x: int
@@ -26,8 +28,9 @@ class BaseUnit(BaseModel):
     damage: float = 10.0
     defense: float = 50
     coord: Point
-    p_crit: float
     radius_dmg: int
+    base_speed: int
+    dmg_coef: float
 
     def damaged(self, damage: float) -> int:
         if self.health > 0:
@@ -43,6 +46,25 @@ class BaseUnit(BaseModel):
             return self.id
         return -1
 
+    def __str__(self):
+        return str(self.id)
+
+
+    async def attack(self, map1: Point, unit):
+        dist = unit.coord - self.coord
+        in_radius = abs(dist.x) < self.radius_dmg and abs(dist.y) < self.radius_dmg
+        if not in_radius:
+            self.move_to(unit, map1, 2)
+            dist = unit.coord - self.coord
+        if abs(dist.x) < self.radius_dmg and abs(dist.y) < self.radius_dmg:
+            p = random.choice(list(range(5)))
+            match p:
+                case 0 | 1: return None
+                case 2 | 3: return unit.damaged(self.damage)
+                case 4:
+                    damage = self.damage * self.dmg_coef
+                    return unit.damaged(damage)
+
 
     def move_to(self, unit, map1: Point, speed: int = 1):
         p_move = random.random()
@@ -53,61 +75,16 @@ class BaseUnit(BaseModel):
             y_move = True
 
         p_move = random.random()
-        if p_move < 0.2:
-            if x_move:
-                if unit.coord.x < self.coord.x:
-                    self.coord.x = self.coord.x - 2 * speed
-                else:
-                    self.coord.x = self.coord.x + 2 * speed
+        if x_move:
+            if unit.coord.x < self.coord.x:
+                self.coord.x = self.coord.x - 2 * speed
             else:
-                if unit.coord.y < self.coord.y:
-                    self.coord.y = self.coord.y - 2 * speed
-                else:
-                    self.coord.y = self.coord.y + 2 * speed
-        elif p_move < 0.4:
-            if x_move:
-                if unit.coord.x < self.coord.x:
-                    self.coord.x = self.coord.x - 4 * speed
-                else:
-                    self.coord.x = self.coord.x + 4 * speed
-            else:
-                if unit.coord.y < self.coord.y:
-                    self.coord.y = self.coord.y - 4 * speed
-                else:
-                    self.coord.y = self.coord.y + 4 * speed
-        elif p_move < 0.6:
-            if x_move:
-                if unit.coord.x < self.coord.x:
-                    self.coord.x = self.coord.x - 8 * speed
-                else:
-                    self.coord.x = self.coord.x + 8 * speed
-            else:
-                if unit.coord.y < self.coord.y:
-                    self.coord.y = self.coord.y - 8 * speed
-                else:
-                    self.coord.y = self.coord.y + 8 * speed
-        elif p_move < 0.8:
-            if x_move:
-                if unit.coord.x < self.coord.x:
-                    self.coord.x = self.coord.x - 12 * speed
-                else:
-                    self.coord.x = self.coord.x + 12 * speed
-            else:
-                if unit.coord.y < self.coord.y:
-                    self.coord.y = self.coord.y - 12 * speed
-                else:
-                    self.coord.y = self.coord.y + 12 * speed
+                self.coord.x = self.coord.x + 2 * speed
         else:
-            if x_move:
-                if unit.coord.x < self.coord.x:
-                    self.coord.x = self.coord.x - 15 * speed
-                else:
-                    self.coord.x = self.coord.x + 15 * speed
+            if unit.coord.y < self.coord.y:
+                self.coord.y = self.coord.y - 2 * speed
             else:
-                if unit.coord.y < self.coord.y:
-                    self.coord.y = self.coord.y - 15 * speed
-                else:
-                    self.coord.y = self.coord.y + 15 * speed
+                self.coord.y = self.coord.y + 2 * speed
 
         if self.coord.x < 0:
             self.coord.x = 1
@@ -120,10 +97,12 @@ class BaseUnit(BaseModel):
 
 
 class Warrior(BaseUnit):
-    p_crit: float = 0.7
     radius_dmg: float = 10
+    base_speed: int = 2
+    dmg_coef: float = 1.5
 
-    def attack(self, map1: Point, unit: BaseUnit) -> int | None:
+    '''
+        def attack(self, map1: Point, unit: BaseUnit) -> int | None:
         dist = unit.coord - self.coord
         in_radius = abs(dist.x) < self.radius_dmg and abs(dist.y) < self.radius_dmg
         if not in_radius:
@@ -138,13 +117,16 @@ class Warrior(BaseUnit):
                 return unit.damaged(self.damage)
             else:
                 return None
+    '''
 
 
 class Archer(BaseUnit):
-    p_crit: float = 0.85
     radius_dmg: float = 20
+    base_speed: int = 2
+    dmg_coef: float = 1.5
 
-    def attack(self, map1: Point, unit: BaseUnit) -> int | None:
+    '''
+        def attack(self, map1: Point, unit: BaseUnit) -> int | None:
         dist = Point(x=unit.coord.x-self.coord.x, y=unit.coord.y-self.coord.y)
         in_radius = abs(dist.x) < self.radius_dmg and abs(dist.y) < self.radius_dmg
         if not in_radius:
@@ -159,13 +141,16 @@ class Archer(BaseUnit):
                 return unit.damaged(self.damage)
             else:
                 return None
+    '''
 
 
 class Varvar(BaseUnit):
-    p_crit: float = 0.9
     radius_dmg: float = 8
+    base_speed: int = 2
+    dmg_coef: float = 2.5
 
-    def attack(self, map1: Point, unit: BaseUnit) -> int | None:
+    '''
+        def attack(self, map1: Point, unit: BaseUnit) -> int | None:
         dist = unit.coord - self.coord
         in_radius = abs(dist.x) < self.radius_dmg and abs(dist.y) < self.radius_dmg
         if not in_radius:
@@ -180,6 +165,7 @@ class Varvar(BaseUnit):
                 return unit.damaged(self.damage)
             else:
                 return None
+    '''
 
 
 class Army(BaseModel):
@@ -187,3 +173,19 @@ class Army(BaseModel):
     name: str
     count: int
     units: dict[int, Warrior | Archer | Varvar] = dict()
+
+    def add_unit(self, count: int, voins: list[BaseUnit]):
+        print("\nVoins in add_unit func:  ", [str(v) for v in voins])
+        print(f"Count of voins: {len(voins)}")
+
+        while count > 0:
+            try:
+                ind = random.randint(0, len(voins) - 1)
+            except ValueError as ve:
+                print(f"Count of voins: {len(voins)}")
+                logger.error(message="Count of voins less or equal 0")
+                return
+            self.units[voins[ind].id] = voins[ind]
+            self.count += 1
+            voins.pop(ind)
+            count -= 1
